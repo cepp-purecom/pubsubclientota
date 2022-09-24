e504a0c8f231fa911b1a888933e9666ebfb3b039
cepp-purecom <82078867+cepp-purecom@users.noreply.github.com>
2022-09-24 13:04:35 +0200

    Rename

--- src/PubSubClientOTA.h
+++ src/PubSubClientOTA.h
@@ -4,8 +4,8 @@
   http://knolleary.net
 */
 
-#ifndef PubSubClient_h
-#define PubSubClient_h
+#ifndef PubSubClientOTA_h
+#define PubSubClientOTA_h
 
 #if defined(ESP8266) || defined(ESP32)
 #include <functional>
